58b99e53330345019790ce720138cb2d486e3d8e
Daan <daan.wendelen@cegeka.be>
2016-05-12 13:34:38 +0200

    Fix some imports

--- 5-sagas/src/test/java/axon/core/UserTest.java
+++ 5-sagas/src/test/java/axon/core/UserTest.java
@@ -4,16 +4,12 @@ import axon.core.command.RegisterUserCommand;
 import axon.core.command.UpdateEmailAddressCommand;
 import axon.core.event.EmailAddressUpdatedEvent;
 import axon.core.event.UserRegisteredEvent;
-import org.axonframework.eventsourcing.annotation.AggregateIdentifier;
 import org.axonframework.test.FixtureConfiguration;
 import org.axonframework.test.Fixtures;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.lang.reflect.Field;
 import java.util.UUID;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class UserTest {
     public static final String NAME = "name";
