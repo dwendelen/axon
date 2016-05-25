e6c392954cd45f9ed8550c4218fc47904e963a6f
Daan <daan.wendelen@cegeka.be>
2016-05-25 23:50:50 +0200

    Make a little demo

--- 5-sagas-sln/src/main/java/axon/core/user/User.java
+++ 5-sagas-sln/src/main/java/axon/core/user/User.java
@@ -1,33 +1,19 @@
 package axon.core.user;
 
 import axon.core.user.command.BuyGameCommand;
-import axon.core.user.command.LinkSteamAccountCommand;
 import axon.core.user.command.RegisterUserCommand;
-import axon.core.user.command.UpdateEmailAddressCommand;
-import axon.core.user.event.EmailAddressUpdatedEvent;
 import axon.core.user.event.GameBoughtEvent;
-import axon.core.user.event.SteamAccountLinkedEvent;
 import axon.core.user.event.UserRegisteredEvent;
-import axon.core.user.exception.GameAlreadyBoughtException;
 import org.axonframework.commandhandling.annotation.CommandHandler;
-import org.axonframework.common.Assert;
 import org.axonframework.eventsourcing.annotation.AbstractAnnotatedAggregateRoot;
 import org.axonframework.eventsourcing.annotation.AggregateIdentifier;
 import org.axonframework.eventsourcing.annotation.EventSourcingHandler;
 
-import java.util.HashSet;
-import java.util.Set;
 import java.util.UUID;
 
 public class User extends AbstractAnnotatedAggregateRoot<UUID> {
     @AggregateIdentifier
     private UUID userId;
-
-    private String name;
-    private String emailAddress;
-
-    private Set<UUID> boughtGames = new HashSet<>();
-    private String steamId;
 
     public User() {}
 
@@ -37,87 +23,29 @@ public class User extends AbstractAnnotatedAggregateRoot<UUID> {
         String name = registerUserCommand.getName();
         String emailAddress = registerUserCommand.getEmailAddress();
 
-        Assert.notNull(userId, "UserId can not be null");
-        Assert.notEmpty(name, "Name can not be empty");
-        Assert.notEmpty(emailAddress, "Email address can not be empty");
-
         UserRegisteredEvent userRegisteredEvent = new UserRegisteredEvent(userId, name, emailAddress);
         apply(userRegisteredEvent);
     }
 
-    public UUID getUserId() {
-        return userId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getEmailAddress() {
-        return emailAddress;
-    }
-
-    public Set<UUID> getBoughtGames() {
-        return new HashSet<>(boughtGames);
-    }
-
-    public String getSteamId() {
-        return steamId;
-    }
 
     @EventSourcingHandler
     public void userRegistered(UserRegisteredEvent userRegisteredEvent) {
         this.userId = userRegisteredEvent.getUserId();
-        this.name = userRegisteredEvent.getName();
-        this.emailAddress = userRegisteredEvent.getEmailAddress();
-    }
-
-    @CommandHandler
-    public void updateEmailAddress(UpdateEmailAddressCommand updateEmailAddressCommand) {
-        UUID userId = updateEmailAddressCommand.getUserId();
-        String newEmailAddress = updateEmailAddressCommand.getNewEmailAddress();
-
-        Assert.notEmpty(newEmailAddress, "The new email address can not be empty");
-
-        EmailAddressUpdatedEvent emailAddressUpdatedEvent = new EmailAddressUpdatedEvent(userId, newEmailAddress);
-        apply(emailAddressUpdatedEvent);
-    }
-
-    @EventSourcingHandler
-    public void emailAddressUpdated(EmailAddressUpdatedEvent emailAddressUpdatedEvent) {
-        this.emailAddress = emailAddressUpdatedEvent.getNewEmailAddress();
     }
 
     @CommandHandler
     public void buyGame(BuyGameCommand buyGameCommand) {
-        if(boughtGames.contains(buyGameCommand.getGameId())) {
-            throw new GameAlreadyBoughtException();
-        }
-
         GameBoughtEvent gameBoughtEvent = new GameBoughtEvent(
                 buyGameCommand.getUserId(),
                 buyGameCommand.getGameId(),
                 buyGameCommand.getPurchaseId());
-        apply(gameBoughtEvent);
+        try {
+            apply(gameBoughtEvent);
+        } catch (Exception e){}
     }
 
     @EventSourcingHandler
     public void handle(GameBoughtEvent gameBoughtEvent) {
-        this.boughtGames.add(gameBoughtEvent.getGameId());
-    }
-
-    @CommandHandler
-    public void linkSteamAccount(LinkSteamAccountCommand linkSteamAccountCommand) {
-        SteamAccountLinkedEvent steamAccountLinkedEvent = new SteamAccountLinkedEvent(
-                linkSteamAccountCommand.getUserId(),
-                linkSteamAccountCommand.getSteamId()
-        );
-
-        apply(steamAccountLinkedEvent);
-    }
-
-    @EventSourcingHandler
-    public void handle(SteamAccountLinkedEvent steamAccountLinkedEvent) {
-        this.steamId = steamAccountLinkedEvent.getSteamId();
+        throw new RuntimeException("Any RuntimeException will do");
     }
 }
--- 5-sagas-sln/src/test/java/axon/core/user/UserTest.java
+++ 5-sagas-sln/src/test/java/axon/core/user/UserTest.java
@@ -1,14 +1,8 @@
 package axon.core.user;
 
 import axon.core.user.command.BuyGameCommand;
-import axon.core.user.command.LinkSteamAccountCommand;
-import axon.core.user.command.RegisterUserCommand;
-import axon.core.user.command.UpdateEmailAddressCommand;
-import axon.core.user.event.EmailAddressUpdatedEvent;
 import axon.core.user.event.GameBoughtEvent;
-import axon.core.user.event.SteamAccountLinkedEvent;
 import axon.core.user.event.UserRegisteredEvent;
-import axon.core.user.exception.GameAlreadyBoughtException;
 import org.axonframework.test.FixtureConfiguration;
 import org.axonframework.test.Fixtures;
 import org.junit.Before;
@@ -20,8 +14,6 @@ public class UserTest {
     public static final String NAME = "name";
     public static final UUID USER_UUID = UUID.randomUUID();
     public static final String OLD_EMAIL = "old email";
-    public static final String NEW_EMAIL = "email";
-    public static final String STEAM_ID = "SteamId";
     public static final UUID GAME_ID = UUID.randomUUID();
 
     private FixtureConfiguration<User> fixture;
@@ -31,77 +23,11 @@ public class UserTest {
         fixture = Fixtures.newGivenWhenThenFixture(User.class);
     }
 
-    //1 USER AGGREGATE
-    @Test
-    public void whenIRegisterAUser_thenAUserIsRegistered() throws Exception {
-        RegisterUserCommand registerUserCommand = new RegisterUserCommand(NAME, NEW_EMAIL);
-
-        UserRegisteredEvent userRegisteredEvent = new UserRegisteredEvent(registerUserCommand.getUserId(), NAME, NEW_EMAIL);
-
-        fixture.given()
-                .when(registerUserCommand)
-                .expectEvents(userRegisteredEvent);
-    }
-
-    @Test
-    public void whenIRegisterAUserWithoutName_thenAnExceptionIsThrown() throws Exception {
-        RegisterUserCommand registerUserCommand = new RegisterUserCommand("", NEW_EMAIL);
-
-        fixture.given()
-                .when(registerUserCommand)
-                .expectException(IllegalArgumentException.class);
-    }
-
-    @Test
-    public void whenIRegisterAUserWithoutAnEmailAddress_thenAnExceptionIsThrown() throws Exception {
-        RegisterUserCommand registerUserCommand = new RegisterUserCommand(NAME, "");
-
-        fixture.given()
-                .when(registerUserCommand)
-                .expectException(IllegalArgumentException.class);
-    }
-
-    @Test
-    public void givenAUser_whenIUpdateTheEmailAddress_thenTheEmailAddressIsUpdated() throws Exception {
-        fixture.given(new UserRegisteredEvent(USER_UUID, NAME, OLD_EMAIL))
-                .when(new UpdateEmailAddressCommand(USER_UUID, NEW_EMAIL))
-                .expectEvents(new EmailAddressUpdatedEvent(USER_UUID, NEW_EMAIL));
-    }
-
-    @Test
-    public void givenAUser_whenIUpdateTheEmailAddressToAnEmptyOne_thenAnExceptionIsThrown() throws Exception {
-        fixture.given(new UserRegisteredEvent(USER_UUID, NAME, OLD_EMAIL))
-                .when(new UpdateEmailAddressCommand(USER_UUID, ""))
-                .expectException(IllegalArgumentException.class);
-    }
-
-    //2 EVENT LISTENERS
-    //3 READ MODELS
-    //4 SPRING
-    //5 SAGAS
-
-    @Test
-    public void givenAUser_whenILinkASteamAccount_thenASteamAccountIsLinked() throws Exception {
-        fixture.given(new UserRegisteredEvent(USER_UUID, NAME, OLD_EMAIL))
-                .when(new LinkSteamAccountCommand(USER_UUID, STEAM_ID))
-                .expectEvents(new SteamAccountLinkedEvent(USER_UUID, STEAM_ID));
-    }
-
     @Test
     public void givenAUser_whenIBuyAGame_thenTheGameIsBought() throws Exception {
         BuyGameCommand buyGameCommand = new BuyGameCommand(USER_UUID, GAME_ID);
         fixture.given(new UserRegisteredEvent(USER_UUID, NAME, OLD_EMAIL))
                 .when(buyGameCommand)
                 .expectEvents(new GameBoughtEvent(USER_UUID, GAME_ID, buyGameCommand.getPurchaseId()));
-    }
-
-    @Test
-    public void givenAUserThatBoughtAGame_whenIBuyThatGameAgain_thenAnExceptionIsThrown() throws Exception {
-        fixture.given(
-                    new UserRegisteredEvent(USER_UUID, NAME, OLD_EMAIL),
-                    new GameBoughtEvent(USER_UUID, GAME_ID, UUID.randomUUID())
-                )
-                .when(new BuyGameCommand(USER_UUID, GAME_ID))
-                .expectException(GameAlreadyBoughtException.class);
     }
 }
